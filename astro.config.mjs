14a42efe2c36e307f61b39a80e80c024ef96030a
Aditya Gopalan <adityagopalan@aditya-laptop.local>
2026-09-08 21:40:20 -0400

    change template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({site: 'https://aditya-s-gopalan.github.io',});
+export default defineConfig({});
